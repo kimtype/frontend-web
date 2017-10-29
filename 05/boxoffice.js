b6a86f98ee47c080d959a335a1ab32e2d0f5b1fc
PureChild <a01056490114@gmail.com>
2017-10-29 16:51:33 +0900

    added button functionality

--- 05/boxoffice.js
+++ 05/boxoffice.js
@@ -1,13 +1,14 @@
-var url = 'http://m.movie.daum.net/data/movie/movie_info/box_office.json?country=KR&startDate=20150101&endDate=20150401&pageSize=3&pageNo='
-
+var baseUrl = 'http://m.movie.daum.net/data/movie/movie_info/box_office.json?country=KR&startDate=20150101&endDate=20150401'
+var pageSize = 3;
+var pageNo = 1;
+var url = '';
 var info = document.getElementById('info');
 var str = '';
-
+url = baseUrl + '&pageSize=' + pageSize + '&pageNo=' + pageNo;
 getJSON(url,done);
 
 function done(result){
   console.log(result);
-
   for(var i = 0; i < result.data.length; i++){
     // console.log(result.data[i].titleKo);
     str += '<h2>'+result.data[i].titleKo+'</h2>';
@@ -15,4 +16,43 @@ function done(result){
   }
 
   info.innerHTML = str;
+}
+
+var control = document.getElementById('control');
+control.innerHTML = '<button id="pre"> < </button>' //이전 버튼
+                  + '<span id="number">'+pageNo+' </span>' //page index
+                  + '<button id="next"> > </button>' //다음 버튼
+var number = document.getElementById('number');
+var pre = document.getElementById('pre');
+var next = document.getElementById('next');
+
+pre.addEventListener('click', prePage);
+next.addEventListener('click', nextPage);
+
+function nextPage() {
+  // if(result.data.length / pageSize == pageNo){
+  //   alert("마지막 페이지입니다.");
+  // }
+  // else{
+    pageNo++;
+    number.innerHTML = pageNo;
+    url = baseUrl + '&pageSize=' + pageSize + '&pageNo=' + pageNo;
+    console.log(url);
+    str = '';
+    getJSON(url,done);
+  // }
+}
+
+function prePage() {
+  console.log('test');
+  if(pageNo == 1){
+    alert("첫 번째 페이지입니다.");
+  }
+  else{
+    pageNo--;
+    number.innerHTML = pageNo;
+    url = baseUrl + '&pageSize=' + pageSize + '&pageNo=' + pageNo;
+    str = '';
+    getJSON(url,done);
+  }
 }
